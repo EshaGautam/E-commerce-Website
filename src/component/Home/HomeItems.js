7423ffd16bd693a7298bbf24591c82c1e1903949
Esha Gautam <eshagautam0@gmail.com>
2024-02-18 16:35:53 +0530

    error handling

--- src/component/Home/HomeItems.js
+++ src/component/Home/HomeItems.js
@@ -1,65 +1,111 @@
-import React, { useState } from 'react'
-import { Container,ListGroup,Button } from 'react-bootstrap';
-import './HomeItems.css'
+import React, { useEffect, useState } from "react";
+import { Container, ListGroup, Button } from "react-bootstrap";
+import "./HomeItems.css";
 
 function HomeItems() {
-  const [movies,setMovies]=useState([])
-  const [loading,setLoading]=useState(false)
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+  const [retrying, setRetrying] = useState(null);
+    const [retryTimeoutId, setRetryTimeoutId] = useState(null);
 
-const handleMovieResponse=async()=>{
-  setLoading(true);
- try {
-    let response = await fetch("https://swapi.dev/api/films/");
-    let movieData = await response.json();
 
-    let transformedData = movieData.results.map((movie) => ({
-      id: movie.episode_id,
-      date: movie.release_date,
-      title: movie.title,
-      director:movie.director,
-    }));
+   
+   const fetchData = async () => {
+         setLoading(true);
+     try {
+       let response = await fetch("https://swapi.dev/api/films/");
 
-    setMovies(transformedData);
-    setLoading(false)
-  } 
-  catch (error) {
-    alert(`Error: ${error.message}`);
-  }
-};
+       if (!response.ok) {
+         throw new Error("Something went wrong, retrying...");
+       }
 
-    const movielist = movies.length>0 && movies.map((movie) => (
-      <Container className="movie">
-        <ListGroup>
-          <div className="movie-list">
-            <ListGroup.Item key={movie.id}>
-              <div className="movie-deets">
-                <span>{movie.date}</span>
-                <span>{movie.title}</span>
-                <span>{movie.director}</span>
-                <span>
-                  <Button variant="primary" className="book-btn">
-                    Book-Now
-                  </Button>
-                </span>
-              </div>
-            </ListGroup.Item>
-          </div>
-        </ListGroup>
-      </Container>
-    ))
-  return (
-    <div>
-      <Button
-        variant="secondary"
-        onClick={handleMovieResponse}
-        className="movie-btn"
-      >
-        Movie
-      </Button>
-      {loading && <p className="onload">Loading...</p> }
-      {movielist}
-    </div>
-  );
-}
+       let movieData = await response.json();
 
-export default HomeItems
+       let transformedData = movieData.results.map((movie) => ({
+         id: movie.episode_id,
+         date: movie.release_date,
+         title: movie.title,
+         director: movie.director,
+       }));
+       setMovies(transformedData);
+     } catch (error) {
+       setError(error.message);
+       if (retrying) {
+         setRetryTimeoutId(setTimeout(fetchData, 5000));
+       }
+     }
+     setLoading(false);
+   };
+
+
+   useEffect(() => {
+     setError(null);
+     if (retrying) {
+       setTimeout(fetchData, 0);
+     }
+   }, [retrying]);
+
+
+
+   const handleCancel = () => {
+     setRetrying(!retrying);
+     clearTimeout(retryTimeoutId);
+     console.log("Retry cancelled");
+   };
+
+   const movielist =
+     movies.length > 0 &&
+     movies.map((movie) => (
+       <Container className="movie" key={movie.id}>
+         <ListGroup>
+           <div className="movie-list">
+             <ListGroup.Item>
+               <div className="movie-deets">
+                 <span>{movie.date}</span>
+                 <span>{movie.title}</span>
+                 <span>{movie.director}</span>
+                 <span>
+                   <Button variant="primary" className="book-btn">
+                     Book-Now
+                   </Button>
+                 </span>
+               </div>
+             </ListGroup.Item>
+           </div>
+         </ListGroup>
+       </Container>
+     ));
+
+   return (
+     <div>
+       <Button
+         type="button"
+         variant="secondary"
+         onClick={() => setRetrying(true)}
+         className="movie-btn"
+       >
+         Movie
+       </Button>
+       {loading && <p className="onload">Loading...</p>}
+       {!loading && movies.length === 0 && (
+         <p className="noloading">Found No Movies </p>
+       )}
+       {!loading && error && (
+         <p className="err-msg">
+           {error}
+           <Button
+             variant="outline-warning"
+             className="cance-btn"
+             onClick={handleCancel}
+           >
+             Cancel
+           </Button>
+         </p>
+       )}
+       {movielist}
+     </div>
+   );
+ };
+
+export default HomeItems;
