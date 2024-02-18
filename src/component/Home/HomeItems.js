741c1c8f0b5e2bc41563d662f1f2b7f1d92fdeae
Esha Gautam <eshagautam0@gmail.com>
2024-02-18 12:14:45 +0530

    Making Api calls

--- src/component/Home/HomeItems.js
+++ src/component/Home/HomeItems.js
@@ -1,59 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container,ListGroup,Button } from 'react-bootstrap';
 import './HomeItems.css'
-const tours = [
-  {
-    id:Math.random(),
-    date: "JUL16",
-    city: "DETROIT,MI",
-    place: "DTE ENERGY MUSIC THEATRE",
-  },
+// const tours = [
+//   {
+//     id:Math.random(),
+//     date: "JUL16",
+//     city: "DETROIT,MI",
+//     place: "DTE ENERGY MUSIC THEATRE",
+//   },
 
-  {
-        id:Math.random(),
-    date: "JUL19",
-    city: "TORONTO,ON",
-    place: "BUDWEISER STAGE",
-  },
-  {
-        id:Math.random(),
-    date: "JUL 22",
-    city: "BRISTOW, VA",
-    place: "JIGGY LUBE LIVE",
-  },
+//   {
+//         id:Math.random(),
+//     date: "JUL19",
+//     city: "TORONTO,ON",
+//     place: "BUDWEISER STAGE",
+//   },
+//   {
+//         id:Math.random(),
+//     date: "JUL 22",
+//     city: "BRISTOW, VA",
+//     place: "JIGGY LUBE LIVE",
+//   },
 
-  {
-        id:Math.random(),
-    date: "JUL 29",
-    city: "PHOENIX, AZ",
-    place: "AK-CHIN PAVILION",
-  },
+//   {
+//         id:Math.random(),
+//     date: "JUL 29",
+//     city: "PHOENIX, AZ",
+//     place: "AK-CHIN PAVILION",
+//   },
 
-  {
-        id:Math.random(),
-    date: "AUG 2",
-    city: "LAS VEGAS, NV",
-    place: "T-MOBILE ARENA",
-  },
-  {
-        id:Math.random(),
-    date: "AUG 7",
-    city: "CONCORD, CA",
-    place: "CONCORD PAVILION",
-  },
-];
+//   {
+//         id:Math.random(),
+//     date: "AUG 2",
+//     city: "LAS VEGAS, NV",
+//     place: "T-MOBILE ARENA",
+//   },
+//   {
+//         id:Math.random(),
+//     date: "AUG 7",
+//     city: "CONCORD, CA",
+//     place: "CONCORD PAVILION",
+//   },
+// ];
 
 function HomeItems() {
+  const [movies,setMovies]=useState([])
 
-    const tourlist = tours.map((tour) => (
-      <Container className="tour">
+const handleMovieResponse=async()=>{
+ try {
+    let response = await fetch("https://swapi.dev/api/films/");
+    let movieData = await response.json();
+
+  
+
+    let transformedData = movieData.results.map((movie) => ({
+      id: movie.episode_id,
+      date: movie.release_date,
+      title: movie.title,
+      director:movie.director,
+    }));
+
+    setMovies(transformedData);
+  } 
+  catch (error) {
+    alert(`Error: ${error.message}`);
+  }
+};
+
+
+
+
+
+    const movielist = movies.map((movie) => (
+      <Container className="movie">
         <ListGroup>
-          <div className="tour-list">
-            <ListGroup.Item key={tour.id}>
-              <div className="tour-deets">
-                <span>{tour.date}</span>
-                <span>{tour.city}</span>
-                <span>{tour.place}</span>
+          <div className="movie-list">
+            <ListGroup.Item key={movie.id}>
+              <div className="movie-deets">
+                <span>{movie.date}</span>
+                <span>{movie.title}</span>
+                <span>{movie.director}</span>
                 <span>
                   <Button variant="primary" className="book-btn">
                     Book-Now
@@ -67,10 +93,17 @@ function HomeItems() {
     ));
   return (
     <div>
-        <h1 className='head'>Tours</h1>
-        {tourlist}
-        </div>
-  )
+      <Button
+        variant="secondary"
+        onClick={handleMovieResponse}
+        className="movie-btn"
+      >
+        Movie
+      </Button>
+     
+      {movielist}
+    </div>
+  );
 }
 
 export default HomeItems
